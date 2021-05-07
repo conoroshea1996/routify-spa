d4a040ca6995dace073599aab91b5dc2f09e5643
conor o shea <conoshea12@gmail.com>
2021-05-07 11:55:10 +0100

    - fix typo

--- src/pages/editor.svelte
+++ src/pages/editor.svelte
@@ -68,113 +68,111 @@
       },
       data: {
         blocks: [
-          [
-            {
-              type: "header",
-              data: {
-                text: "New header",
-                level: 2,
-              },
+          {
+            type: "header",
+            data: {
+              text: "New header",
+              level: 2,
             },
-            {
-              type: "paragraph",
-              data: {
-                text: "Job purpose",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text: "Job purpose",
             },
-            {
-              type: "paragraph",
-              data: {
-                text:
-                  "Provide a brief description of the general nature of the position; an overview of why the job exists; and what the job is to accomplish.",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text:
+                "Provide a brief description of the general nature of the position; an overview of why the job exists; and what the job is to accomplish.",
             },
-            {
-              type: "paragraph",
-              data: {
-                text:
-                  "The job purpose is usually no more than a few sentences long.",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text:
+                "The job purpose is usually no more than a few sentences long.",
             },
-            {
-              type: "paragraph",
-              data: {
-                text: "Duties and responsibilities",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text: "Duties and responsibilities",
             },
-            {
-              type: "paragraph",
-              data: {
-                text:
-                  "List the primary job duties and responsibilities using headings and then give examples of the types of activities under each heading. Identify between three and eight primary duties and responsibilities for the position.",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text:
+                "List the primary job duties and responsibilities using headings and then give examples of the types of activities under each heading. Identify between three and eight primary duties and responsibilities for the position.",
             },
-            {
-              type: "list",
-              data: {
-                style: "unordered",
-                items: [
-                  "List the primary duties and responsibilities in order of importance",
-                  "Begin each statement with an action verb",
-                  "Use the present tense of verbs",
-                  "Use gender neutral language",
-                  "Use generic language",
-                  "Where appropriate use qualifiers to clarify the task – where, when, why or how often – for example instead of “greet visitor to the office” use “greet visitors to the office in a professional and friendly manner”",
-                ],
-              },
+          },
+          {
+            type: "list",
+            data: {
+              style: "unordered",
+              items: [
+                "List the primary duties and responsibilities in order of importance",
+                "Begin each statement with an action verb",
+                "Use the present tense of verbs",
+                "Use gender neutral language",
+                "Use generic language",
+                "Where appropriate use qualifiers to clarify the task – where, when, why or how often – for example instead of “greet visitor to the office” use “greet visitors to the office in a professional and friendly manner”",
+              ],
             },
-            {
-              type: "paragraph",
-              data: {
-                text: "Qualifications",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text: "Qualifications",
             },
-            {
-              type: "paragraph",
-              data: {
-                text:
-                  "State the minimum qualifications required to successfully perform the job.",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text:
+                "State the minimum qualifications required to successfully perform the job.",
             },
-            {
-              type: "paragraph",
-              data: {
-                text: "Qualifications include",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text: "Qualifications include",
             },
-            {
-              type: "list",
-              data: {
-                style: "unordered",
-                items: [
-                  "Education",
-                  "Specialized knowledge",
-                  "Skills",
-                  "Abilities",
-                  "Other characteristics such as personal characteristics",
-                  "Professional Certification",
-                  "Experience",
-                ],
-              },
+          },
+          {
+            type: "list",
+            data: {
+              style: "unordered",
+              items: [
+                "Education",
+                "Specialized knowledge",
+                "Skills",
+                "Abilities",
+                "Other characteristics such as personal characteristics",
+                "Professional Certification",
+                "Experience",
+              ],
             },
-            {
-              type: "paragraph",
-              data: {
-                text: "Perks/Benefits of the role",
-              },
+          },
+          {
+            type: "paragraph",
+            data: {
+              text: "Perks/Benefits of the role",
             },
-            {
-              type: "image",
-              data: {
-                url:
-                  "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
-                caption: "Random Image attached using url",
-                withBorder: false,
-                withBackground: false,
-                stretched: false,
-              },
+          },
+          {
+            type: "image",
+            data: {
+              url:
+                "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
+              caption: "Random Image attached using url",
+              withBorder: false,
+              withBackground: false,
+              stretched: false,
             },
-          ],
+          },
         ],
       },
     });
