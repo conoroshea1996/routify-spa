a8ea6a66dd9784e94919e7fac41acab585c4c002
conor o shea <conoshea12@gmail.com>
2021-05-07 11:53:57 +0100

    - add more plugins to editor (testing)

--- src/pages/editor.svelte
+++ src/pages/editor.svelte
@@ -1,10 +1,15 @@
 <script>
   import EditorJS from "@editorjs/editorjs";
+  import Paragraph from "@editorjs/paragraph";
   import Header from "@editorjs/header";
+  import List from "@editorjs/list";
+  import SimpleImage from "@editorjs/simple-image";
+  import ColorPlugin from "editorjs-text-color-plugin";
   import { onMount } from "svelte";
 
   let editor;
   let editorInstance;
+
   onMount(async () => {
     console.log(editor);
     editorInstance = new EditorJS({
@@ -16,27 +21,160 @@
           class: Header,
           config: {
             placeholder: "Enter a header",
-            levels: [2, 3, 4],
-            defaultLevel: 3,
+            levels: [2, 4],
+            defaultLevel: 2,
+          },
+          inlineToolbar: true,
+        },
+        paragraph: {
+          class: Paragraph,
+          inlineToolbar: true,
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+        image: SimpleImage,
+
+        Color: {
+          class: ColorPlugin,
+          config: {
+            colorCollections: [
+              "#FF1300",
+              "#EC7878",
+              "#9C27B0",
+              "#673AB7",
+              "#3F51B5",
+              "#0070FF",
+              "#03A9F4",
+              "#00BCD4",
+              "#4CAF50",
+              "#8BC34A",
+              "#CDDC39",
+              "#FFF",
+              "#000000",
+            ],
+            defaultColor: "#000000",
+            type: "text",
+          },
+        },
+        Marker: {
+          class: ColorPlugin,
+          config: {
+            defaultColor: "#FFBF00",
+            type: "marker",
           },
         },
       },
       data: {
         blocks: [
-          {
-            type: "header",
-            data: {
-              text: "New header",
-              level: 2,
+          [
+            {
+              type: "header",
+              data: {
+                text: "New header",
+                level: 2,
+              },
             },
-          },
-          {
-            type: "paragraph",
-            data: {
-              text:
-                "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.",
+            {
+              type: "paragraph",
+              data: {
+                text: "Job purpose",
+              },
             },
-          },
+            {
+              type: "paragraph",
+              data: {
+                text:
+                  "Provide a brief description of the general nature of the position; an overview of why the job exists; and what the job is to accomplish.",
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text:
+                  "The job purpose is usually no more than a few sentences long.",
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text: "Duties and responsibilities",
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text:
+                  "List the primary job duties and responsibilities using headings and then give examples of the types of activities under each heading. Identify between three and eight primary duties and responsibilities for the position.",
+              },
+            },
+            {
+              type: "list",
+              data: {
+                style: "unordered",
+                items: [
+                  "List the primary duties and responsibilities in order of importance",
+                  "Begin each statement with an action verb",
+                  "Use the present tense of verbs",
+                  "Use gender neutral language",
+                  "Use generic language",
+                  "Where appropriate use qualifiers to clarify the task – where, when, why or how often – for example instead of “greet visitor to the office” use “greet visitors to the office in a professional and friendly manner”",
+                ],
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text: "Qualifications",
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text:
+                  "State the minimum qualifications required to successfully perform the job.",
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text: "Qualifications include",
+              },
+            },
+            {
+              type: "list",
+              data: {
+                style: "unordered",
+                items: [
+                  "Education",
+                  "Specialized knowledge",
+                  "Skills",
+                  "Abilities",
+                  "Other characteristics such as personal characteristics",
+                  "Professional Certification",
+                  "Experience",
+                ],
+              },
+            },
+            {
+              type: "paragraph",
+              data: {
+                text: "Perks/Benefits of the role",
+              },
+            },
+            {
+              type: "image",
+              data: {
+                url:
+                  "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
+                caption: "Random Image attached using url",
+                withBorder: false,
+                withBackground: false,
+                stretched: false,
+              },
+            },
+          ],
         ],
       },
     });
